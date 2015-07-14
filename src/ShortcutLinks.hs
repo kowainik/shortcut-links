049740a3a666a9ac5c80a58cc266d37ac3f98368
Artyom <yom@artyom.me>
2015-07-14 22:43:28 +0300

    useShortcut

--- src/ShortcutLinks.hs
+++ src/ShortcutLinks.hs
@@ -1,8 +1,30 @@
 module ShortcutLinks
 (
+  Result(..),
   allShortcuts,
 )
 where
 
 
-import ShortcutLinks.All (allShortcuts)
+-- Text
+import Data.Text (Text)
+import qualified Data.Text as T
+-- shortcut-links
+import ShortcutLinks.All (allShortcuts, Result(..))
+
+
+-- | Use a shortcut from 'allShortcuts'.
+--
+-- This is the main function you should use. It does the lookup and so on.
+useShortcut
+  :: Text          -- ^ Shortcut name
+  -> Maybe Text    -- ^ Option
+  -> Text          -- ^ Link text
+  -> Result Text   -- ^ Resulting URL
+useShortcut name option link =
+  let givenShortcut (names,_) = name `elem` names
+      quotedName = "'" ++ T.unpack name ++ "'"
+  in  case filter givenShortcut allShortcuts of
+        []   -> fail ("there's no shortcut named " ++ quotedName)
+        [sh] -> (snd sh) option link
+        _    -> fail ("there's more than one shortcut named " ++ quotedName)
